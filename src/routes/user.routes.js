4ff698e16fffb143110ac8ebe0fb11e6137d21b0
letmeknow908-prog <letmeknow908@gmail.com>
2025-12-24 17:22:46 -0600

    Add KYC routes for submitting and retrieving status

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const kycController = require('../controllers/kyc.controller');
 
+router.post('/kyc', authenticateToken, kycController.submitKYC);
+router.get('/kyc', authenticateToken, kycController.getKYCStatus);
 router.get('/me', (req, res) => {
   res.json({
     status: 'success',
