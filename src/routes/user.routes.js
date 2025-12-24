780e2245d2a0beab45f496260e2f9462af49cccf
letmeknow908-prog <letmeknow908@gmail.com>
2025-12-24 17:36:14 -0600

    Clean up user.routes.js by removing comments
    Removed commented-out user route examples from user.routes.js.

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -10,13 +10,7 @@ const { authenticateToken } = require('../middleware/auth.middleware');
 router.post('/kyc', authenticateToken, kycController.submitKYC);
 router.get('/kyc', authenticateToken, kycController.getKYCStatus);
 
-// Admin KYC routes (optional)
 router.put('/kyc/:userId/approve', authenticateToken, kycController.approveKYC);
 router.put('/kyc/:userId/reject', authenticateToken, kycController.rejectKYC);
-
-// If you have other user routes, add them here
-// Example:
-// router.get('/me', authenticateToken, userController.getProfile);
-// router.put('/me', authenticateToken, userController.updateProfile);
 
 module.exports = router;
